0ac1630cc3d97824a1538532b5dcf751bdaad198
Shorotshishir <tanzim121@gmail.com>
2019-01-23 21:24:47 +0600

    'deploy'

---
+++ precache-manifest.8b728ba3f48b963b97a5c4e15369f492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c98906c62e9b0564cc06",
+    "url": "/react-deploy-test/static/js/runtime~main.c98906c6.js"
+  },
+  {
+    "revision": "0c9d6e74084dfb49566f",
+    "url": "/react-deploy-test/static/js/main.0c9d6e74.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-deploy-test/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "0c9d6e74084dfb49566f",
+    "url": "/react-deploy-test/static/css/main.13d27162.chunk.css"
+  },
+  {
+    "revision": "f2aed6c4944864049cb33b10baa047bc",
+    "url": "/react-deploy-test/index.html"
+  }
+];
